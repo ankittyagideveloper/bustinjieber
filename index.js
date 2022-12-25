2ed714e1fbdbfdbcd7540c42eed1b02c42ba7f50
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2022-12-25 21:17:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+
+let lastHole;
+let timeup = false;
+let score = 0;
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeup) peep();
+  }, time);
+}
+function startGame() {
+  score = 0;
+  scoreBoard.textContent = 0;
+  timeup = false;
+  peep();
+  setTimeout(() => (timeup = true), 10000);
+}
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+moles.forEach((moles) => moles.addEventListener("click", bonk));
